a24680e8c510b4ad27a7c6f68dbb5b341189f459
wuliguaiguaia <1944063509@qq.com>
2018-11-24 03:04:55 +0800

    :sparkles:image mimic send request

--- imgRequest/server.js
+++ imgRequest/server.js
@@ -3,21 +3,19 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if (!port) {
-    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
-    process.exit(1)
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
 }
 
-var server = http.createServer(function (request, response) {
-    var parsedUrl = url.parse(request.url, true)
-    var path = request.url
-    var query = ''
-    if (path.indexOf('?') >= 0) {
-        query = path.substring(path.indexOf('?'))
-    }
-    var pathNoQuery = parsedUrl.pathname
-    var queryObject = parsedUrl.query
-    var method = request.method
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
+  var method = request.method
 
     /******** 从这里开始看，上面不要看 ************/
 
